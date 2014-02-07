b14ce0d1acfcc2d51af01b82637fbaefe4823c98
Manuel Kehl <mank319@zoho.com>
2014-02-07 15:38:35 +0100

    Added a check if function go_child_menu exists to avoid php errors, if the Plugin is not available

--- template.php
+++ template.php
@@ -29,7 +29,9 @@
 		    First check if go_child_menu is availabe, so the theme does not
 		    cause php Errors, if the Child Menu Plugin is unavailable
 		  */
-		  go_child_menu();
+		  if (function_exists('go_child_menu')) {
+		    go_child_menu();
+		  }
                 ?>
             </ul>
         </nav>
